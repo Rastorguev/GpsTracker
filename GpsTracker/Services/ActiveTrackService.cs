2cd2d2bbbb03ca43044e1fa083cac49105e5691c
Rastorguev <RastorguevS@gmail.com>
2014-09-23 12:36:55 +0300

    Reduce number of track points to store

--- GpsTracker/Services/ActiveTrackService.cs
+++ GpsTracker/Services/ActiveTrackService.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using Android.App;
 using Android.Content;
-using Android.Gms.Maps.Model;
 using Android.Locations;
 using Android.OS;
 using GpsTracker.Entities;
@@ -13,19 +12,11 @@ namespace GpsTracker.Services
     [Service]
     public class ActiveTrackService : Service
     {
+        private const float MinValuableBearing = 1;
+
         private DateTime _startTime;
 
         public TrackData ActiveTrack { get; private set; }
-
-        public void AddTrackPoint(LatLng trackPoint)
-        {
-            if (ActiveTrack.TrackPoints.Any())
-            {
-                ActiveTrack.Distance += ActiveTrack.TrackPoints.Last().ToLatLng().DistanceTo(trackPoint);
-            }
-
-            ActiveTrack.TrackPoints.Add(trackPoint.ToTrackPoint());
-        }
 
         public override IBinder OnBind(Intent intent)
         {
@@ -39,12 +30,11 @@ namespace GpsTracker.Services
             ActiveTrack = new TrackData(_startTime);
 
             var location = App.LocationListener.Location;
-            var startPosition = location != null ? location.ToLatLng() : null;
 
             //GeneratedFakeTrack(60000);
 
-            AddTrackPoint(startPosition);
-            
+            UpdateTrackPoints(location);
+
             StartForeground((int) NotificationFlags.ForegroundService,
                 TrackRecordingNotifications.GetRecordStartedNotification(this));
 
@@ -70,7 +60,47 @@ namespace GpsTracker.Services
 
         public virtual void OnLocationChanged(Location location)
         {
-            AddTrackPoint(location.ToLatLng());
+            UpdateTrackPoints(location);
+        }
+
+        private void UpdateTrackPoints(Location location)
+        {
+            if (NeedToAddNewTrackPoint(location))
+            {
+                AddNewTrackPoint(location);
+            }
+            else
+            {
+                UpdateLastTrackPoint(location);
+            }
+        }
+
+        private void AddNewTrackPoint(Location location)
+        {
+            if (ActiveTrack.TrackPoints.Any())
+            {
+                ActiveTrack.Distance += ActiveTrack.TrackPoints.Last().ToLatLng().DistanceTo(location.ToLatLng());
+            }
+
+            ActiveTrack.TrackPoints.Add(location.ToTrackPoint());
+        }
+
+        private void UpdateLastTrackPoint(Location location)
+        {
+            var lastTrackPoint = ActiveTrack.TrackPoints[ActiveTrack.TrackPoints.Count - 1];
+            var lastButOneTrackPoint = ActiveTrack.TrackPoints[ActiveTrack.TrackPoints.Count - 2];
+
+            ActiveTrack.Distance -= lastButOneTrackPoint.ToLatLng().DistanceTo(lastTrackPoint.ToLatLng());
+            ActiveTrack.Distance += lastButOneTrackPoint.ToLatLng().DistanceTo(location.ToLatLng());
+
+            ActiveTrack.TrackPoints[ActiveTrack.TrackPoints.Count - 1] = location.ToTrackPoint();
+        }
+
+        private bool NeedToAddNewTrackPoint(Location location)
+        {
+            return App.LocationListener.PreviousLocation == null ||
+                   ActiveTrack.TrackPoints.Count < 2 ||
+                   Math.Abs(App.LocationListener.PreviousLocation.Bearing - location.Bearing) > MinValuableBearing;
         }
 
         private void GeneratedFakeTrack(int n)
@@ -83,7 +113,7 @@ namespace GpsTracker.Services
             {
                 lat += 0.000008;
 
-                var x = (double)1 / random.Next(-100000, 100000);
+                var x = (double) 1/random.Next(-100000, 100000);
 
                 ts.Add(new TrackPoint(lat, 27.689841 + x));
             }
@@ -94,13 +124,13 @@ namespace GpsTracker.Services
 
     public class ActiveTrackServiceBinder : Binder
     {
-        public ActiveTrackService Service { get; private set; }
-
-        public bool IsBound { get; set; }
-
         public ActiveTrackServiceBinder(ActiveTrackService service)
         {
             Service = service;
         }
+
+        public ActiveTrackService Service { get; private set; }
+
+        public bool IsBound { get; set; }
     }
 }
